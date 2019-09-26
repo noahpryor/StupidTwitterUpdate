c347c2fd24a57185b3c6075bbaa0cfa3be7215de
Kevin DeLand <kevin.deland@gmail.com>
2019-09-25 21:19:50 -0400

    initial commit with all the garbage

---
+++ README.md
@@ -0,0 +1,12 @@
+# Twitter Update
+see here:
+https://twitter.com/fellawhomstdve/status/1176954306525880320
+
+## Dev Instructions
+1. Download or clone this folder into a local directory of your choice.
+2. Open your Google Chrome browser and navigate to "chrome://extensions/".
+3. Click on "Load unpacked extension".
+4. Navigate to your location of this project and press "Select".
+5. Make sure that the "Twitter Update" extension is Enabled. You should see the icon has been added to your Chrome toolbar.
+6. Use Chrome to navigate to Twitter.
+
---
+++ script.js
@@ -0,0 +1,30 @@
+
+window.doItForTheMemes = function(){
+  // Liv Fanon if u see this dm me i think ur cute
+  $( "a[href='/home'] > div > div > span").text("Garbage");
+  $( "a[href='/explore'] > div > div > span").text("Harass");
+  $( "a[href='/notifications'] > div > div > span").text("Get Harassed");
+  $( "a[href='/messages'] > div > div > span").text("The Boys");
+}
+
+window.onload = function () {
+  setTimeout(window.doItForTheMemes, 300);
+  // idk why the fuck it's overwriting this... this is good enough for now
+  setTimeout(window.doItForTheMemes, 1000);
+}
+
+// https://stackoverflow.com/questions/641857/javascript-window-resize-event
+var addEvent = function(object, type, callback) {
+  if (object == null || typeof(object) == 'undefined') return;
+  if (object.addEventListener) {
+      object.addEventListener(type, callback, false);
+  } else if (object.attachEvent) {
+      object.attachEvent("on" + type, callback);
+  } else {
+      object["on"+type] = callback;
+  }
+};
+
+addEvent(window, "resize", function(event) {
+  setTimeout(window.doItForTheMemes, 500);
+});
